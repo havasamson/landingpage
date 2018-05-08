b310ce1c149fafd688a051740aeeed3897f03054
Aleksandra Samsonovitch <aleksandra@Aleksandras-MacBook-Pro.local>
2018-05-08 20:36:45 +0300

    nuu

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var server  = require('http').createServer(app);
 //app.configure, app.use etc
 
 app.listen(process.env.PORT || '8000', function(){
